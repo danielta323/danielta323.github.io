0d6cb6deb918f5736fed0875221a85680f4fb886
Daniel Yang <daniel.yang@kaiostech.com>
2020-06-23 17:36:03 +0800

    add window load event listener

--- postMessage/child.js
+++ postMessage/child.js
@@ -1,24 +1,26 @@
-var display = document.getElementById('displaymessage');
+window.addEventListener('load', () => {
+  window.display = document.getElementById('displaymessage');
 
-function receiveMessage(event)
-{
-  // Do we trust the sender of this message?
-  if (event.origin !== "https://danielta323.github.io")
-    return;
+  function receiveMessage(event)
+  {
+    // Do we trust the sender of this message?
+    if (event.origin !== "https://danielta323.github.io")
+      return;
 
-  // event.source is window.opener
-  // event.data is "hello there!"
+    // event.source is window.opener
+    // event.data is "hello there!"
 
-  // Assuming you've verified the origin of the received message (which
-  // you must do in any case), a convenient idiom for replying to a
-  // message is to call postMessage on event.source and provide
-  // event.origin as the targetOrigin.
-  event.source.postMessage(
-    "hi there yourself!  the secret response is: rheeeeet!",
-    event.origin
-  );
-  console.log('event', event);
-  // display.innerText = event.detail.message;
-}
+    // Assuming you've verified the origin of the received message (which
+    // you must do in any case), a convenient idiom for replying to a
+    // message is to call postMessage on event.source and provide
+    // event.origin as the targetOrigin.
+    event.source.postMessage(
+      "hi there yourself!  the secret response is: rheeeeet!",
+      event.origin
+    );
+    console.log('event', event);
+    // display.innerText = event.detail.message;
+  }
 
-window.addEventListener("message", receiveMessage, false);
+  window.addEventListener("message", receiveMessage, false);
+});
--- postMessage/parent.js
+++ postMessage/parent.js
@@ -1,18 +1,15 @@
-/*
- * A窗口的域名是<http://example.com:8080>，以下是A窗口的script标签下的代码：
- */
+window.addEventListener('load', () => {
+  window.popup = window.open("https://danielta323.github.io/postMessage/child.html");
 
-var popup = window.open("https://danielta323.github.io/postMessage/child.html");
+  window.popup.postMessage("The user is 'bob' and the password is 'secret'",
+                    "https://danielta323.github.io");
 
-popup.postMessage("The user is 'bob' and the password is 'secret'",
-                  "https://danielta323.github.io");
+  window.popup.postMessage("hello there!", "https://danielta323.github.io");
 
-popup.postMessage("hello there!", "https://danielta323.github.io");
-
-function receiveMessage(event)
-{
-  if (event.origin !== "https://danielta323.github.io")
-    return;
-
-}
-window.addEventListener("message", receiveMessage, false);
+  function receiveMessage(event)
+  {
+    if (event.origin !== "https://danielta323.github.io")
+      return;
+  }
+  window.addEventListener("message", receiveMessage, false);
+});
