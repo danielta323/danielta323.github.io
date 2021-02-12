dc244ac68e6ad298db3accc3f060ee92e83ec8f4
Daniel.Yang <danielta323@gmail.com>
2021-02-13 00:04:47 +0800

    Add some openlayer try out

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HelloWorld class="hello-world" msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+  width: 100vw;
+  height: 100vh;
+}
+
+.hello-world{
+  height: 100px;
+}
+</style>
