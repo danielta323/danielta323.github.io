ce34ff1c6ef38710fd012edede57a18ff237cc96
Daniel Yang <daniel.yang@kaiostech.com>
2021-03-23 17:05:15 +0800

    complete opener tests

--- recursive_opener.js
+++ recursive_opener.js
@@ -1,13 +1,15 @@
 var noOpenerBtn = document.getElementById('no_opener_btn');
 noOpenerBtn.addEventListener('click', () => {
-  var popup = window.open('https://danielta323.github.io/recursive_opener.html', '_blank');
-  popup.postMessage("aaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://danielta323.github.io");
+  window.popupWindow = window.open('https://danielta323.github.io/recursive_opener.html', '_blank');
+  window.popupWindow.postMessage("aaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://danielta323.github.io");
 });
 
 window.addEventListener("message", (event) => {
-  if (event.origin !== "https://danielta323.github.io")
-    return;
-
+  console.log('message', event);
   console.log('event.source', event.source);
   console.log('event.data', event.data);
 }, false);
+
+if (window.opener) {
+  window.opener.postMessage('yes! I have a window.opener!!!');
+}
